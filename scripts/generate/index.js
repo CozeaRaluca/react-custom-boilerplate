da06ab17d364c7ddd829f554ff382e27d5d6d13a
CozeaRaluca <cozea.raluca@yahoo.com>
2021-01-31 21:25:57 +0200

    Add data-testid attribute.

--- scripts/generate/index.js
+++ scripts/generate/index.js
@@ -1,7 +1,9 @@
 const componentGenerator = require('./component');
 const contextGenerator = require('./context');
+const pageGenerator = require('./page');
 
 module.exports = plop => {
   plop.setGenerator('component', componentGenerator);
   plop.setGenerator('context', contextGenerator);
+  plop.setGenerator('page', pageGenerator);
 };
